2dd5f99d79a0bfb8a06d4c5a99296bd26f90b59c
echo8 <greg@echo8.dev>
2024-11-06 20:23:40 +0900

    fix root dir util

--- tests/internal/testutil/util.go
+++ tests/internal/testutil/util.go
@@ -1,21 +1,23 @@
 package testutil
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 func Ptr[T any](value T) *T {
 	return &value
 }
 
 func ProjectRootDir() string {
-	// dir, err := os.Getwd()
-	// if err != nil {
-	// 	panic(fmt.Errorf("failed to find project root dir: %w", err))
-	// }
-	// pathSplit := strings.Split(dir, string(filepath.Separator))
-	// if len(pathSplit) > 2 && pathSplit[len(pathSplit)-1] == "internal" &&
-	// 	pathSplit[len(pathSplit)-2] == "e2e" {
-	// 	return string(filepath.Separator) + filepath.Join(pathSplit[0:len(pathSplit)-2]...)
-	// } else if len(pathSplit) > 1 && pathSplit[len(pathSplit)-1] == "e2e" {
-	// 	return string(filepath.Separator) + filepath.Join(pathSplit[0:len(pathSplit)-1]...)
-	// }
-	// return dir
-	return "/home/greg/workspace/krp"
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(fmt.Errorf("failed to find project root dir: %w", err))
+	}
+	krpIdx := strings.LastIndex(dir, "krp")
+	if krpIdx == -1 {
+		panic(fmt.Errorf("failed to find project root dir from: %s", dir))
+	}
+	return dir[:krpIdx+3]
 }
